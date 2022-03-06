c5fbee29d5e9e239b9263b87b6829647a5a0fbca
isikhalil <isikhalil.mail@gmail.com>
2022-03-06 22:02:31 +0000

    Haver ve makale sayfalari eklendi

--- src/app/screens/analysis/calculator/calculator.page.ts
+++ src/app/screens/analysis/calculator/calculator.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.page.scss'],
 })
 export class CalculatorPage implements OnInit {
-
+   num:number;
+   res:number;
   constructor() { }
 
   ngOnInit() {
   }
-
+  calc(){
+    console.log("s")
+    console.log(this.num)
+    console.log(this.res)
+    this.res=this.num;
+  }
 }
