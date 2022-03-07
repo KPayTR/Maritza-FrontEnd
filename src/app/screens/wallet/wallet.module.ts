192c39700be71612a87acbf2fe02201772af95a0
yakupkocaman <me@yakupkocaman.com>
2022-03-07 02:33:03 +0200

    Cüzdan ekranı eklendi

--- src/app/screens/wallet/wallet.module.ts
+++ src/app/screens/wallet/wallet.module.ts
@@ -2,16 +2,20 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
 import { Wallet } from './wallet.page';
 
 import { WalletRoutingModule } from './wallet-routing.module';
+import { SharedComponentsModule } from 'src/app/components/shared-components.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    WalletRoutingModule
+    WalletRoutingModule,
+    NgApexchartsModule,
+    SharedComponentsModule
   ],
   declarations: [Wallet]
 })
--- src/app/screens/wallet/wallet.page.ts
+++ src/app/screens/wallet/wallet.page.ts
@@ -1,4 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  ChartComponent,
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart,
+  ApexPlotOptions,
+  ApexDataLabels,
+  ApexFill
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+  plotOptions: ApexPlotOptions;
+  dataLabels: ApexDataLabels;
+  fill: ApexFill;
+};
 
 @Component({
   selector: 'app-wallet',
@@ -7,6 +27,122 @@ import { Component } from '@angular/core';
 })
 export class Wallet {
 
-  constructor() {}
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
+  assets: any = [
+    {
+      symbol: 'Altın',
+      isPrimary: false,
+      price: 1200,
+      amountGR: 10,
+      amountTRY: 20000,
+      color: 'rgba(248, 192, 47, 1)'
+    },
+    {
+      symbol: 'Gümüş',
+      isPrimary: false,
+      price: 1200,
+      amountGR: 10,
+      amountTRY: 3000,
+      color: 'rgba(234, 234, 234)'
+    },
+    {
+      symbol: 'Paladyum',
+      isPrimary: false,
+      price: 1200,
+      amountGR: 10,
+      amountTRY: 5000,
+      color: 'rgba(186, 181, 181)'
+    },
+    {
+      symbol: 'Platin',
+      isPrimary: false,
+      price: 1200,
+      amountGR: 10,
+      amountTRY: 6000,
+      color: 'rgba(55, 54, 54, 1)'
+    },
+    {
+      symbol: 'Nakit',
+      isPrimary: true,
+      price: 0,
+      amountGR: 0,
+      amountTRY: 10000,
+      color: 'rgba(2, 117, 74)'
+    }
+  ];
+  series: any = [];
+  colors: any = [];
+  selectedItem: any;
+  selectedSegment = 'assets';
+
+  constructor(
+    private router: Router
+  ) {
+    this.chartOptions = {
+      chart: {
+        type: "donut",
+        events: {
+          dataPointSelection: (event, chartContext, config) => {
+            this.selectedItem = this.assets[config.dataPointIndex];
+          }
+        }
+      },
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 300
+            },
+            legend: {
+              show: false
+            }
+          }
+        }
+      ],
+      fill: {
+        type: "gradient"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            labels: {
+              show: false,
+              name: {
+                color: 'orange'
+              }
+            }
+          }
+        }
+      }
+    };
+  }
+
+  ionViewDidEnter() {
+    this.colors= this.assets.map(x => x.color);
+    this.series = this.assets.map(x => x.amountTRY);;
+  }
+
+  segmentChange(e) {
+    this.selectedSegment = e.detail.value;
+  }
+
+  goBuy(){
+    this.router.navigate(['/buySell/buy'])
+  }
+  goSell(){
+    this.router.navigate(['/buySell/sell'])
+  }
+  goWithdraw(){
+    this.router.navigate(['/transfer/deposit-credit-card'])
+  }
+  goDeposit(){
+    this.router.navigate(['/transfer/withdraw-account'])
+  }
 }
